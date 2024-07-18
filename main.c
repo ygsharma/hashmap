4f55bca471043849b7c96bb9e54b608a94c6e2b6
Yogesh Sharma <yogesh.sharma.007321@gmail.com>
2024-07-18 11:50:03 -0400

    first commit

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "hashmap.h"
+
+void printEntry(void* key, void* val) {
+    printf("%s --> %s\n", (char*)key, val ? (char*)val : "NULL");
+}
+
+int main() {
+
+    hashmap m = hmap_allocateStrAsKey();
+    
+    printf("%d\n", m.numBuckets);
+
+    hmap_put(&m, "key1", "val1");
+    hmap_put(&m, "key2", "val2");
+    hmap_put(&m, "key3", "val3");
+    hmap_put(&m, "key4", "val4");
+    hmap_put(&m, "key5", "val5");
+    hmap_put(&m, "key6", "val6");
+    hmap_put(&m, "key7", "val7");
+    hmap_put(&m, "key8", "val8");
+    hmap_put(&m, "key9", "val9");
+    hmap_put(&m, "keya", "vala");
+    hmap_put(&m, "keyb", "valb");
+    hmap_put(&m, "keyc", "valc");
+    hmap_put(&m, "keyd", "vald");
+    hmap_put(&m, "keye", "vale");
+    hmap_put(&m, "keyf", "valf");
+
+    printf("%d\n", m.numBuckets);
+
+
+    hmap_traverse(&m, printEntry);
+    hmap_freeDeep(&m);
+ 
+    return 0;
+}
